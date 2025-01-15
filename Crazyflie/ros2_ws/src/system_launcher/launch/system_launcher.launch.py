2db95ecd8401115eeb2ffe177e7889b8ee848c0a
Aljaz <aljaz.grdadolnik1@gmail.com>
2025-01-15 13:52:23 +0000

    Ground robot with crazyflies in gazebo, problem with topics

--- Crazyflie/ros2_ws/src/system_launcher/launch/system_launcher.launch.py
+++ Crazyflie/ros2_ws/src/system_launcher/launch/system_launcher.launch.py
@@ -8,19 +8,21 @@ from launch.conditions import LaunchConfigurationEquals
 
 
 def generate_launch_description():
-    # load crazyflies
+    pkg_system_launcher = get_package_share_directory("system_launcher")
+    pgk_crazyflie = get_package_share_directory("crazyflie")
+    sdf_robot_file = os.path.join(pkg_system_launcher, "models", "robot.sdf")
+    
+    # load Crazyflie configuaration
     crazyflies_yaml = os.path.join(
-        get_package_share_directory("system_launcher"), "config", "crazyfliesConfig.yaml"
+        pkg_system_launcher, "config", "crazyfliesConfig.yaml"
     )
-
     with open(crazyflies_yaml, "r") as ymlfile:
         crazyflies = yaml.safe_load(ymlfile)
 
-    # server params
+    # load server configuration
     server_yaml = os.path.join(
-        get_package_share_directory("system_launcher"), "config", "server.yaml"
+        pkg_system_launcher, "config", "server.yaml"
     )
-
     with open(server_yaml, "r") as ymlfile:
         server_yaml_contents = yaml.safe_load(ymlfile)
 
@@ -28,14 +30,19 @@ def generate_launch_description():
         server_yaml_contents["/crazyflie_server"]["ros__parameters"]
     ]
 
-    # robot description
+    # load Crazyflie robot description
     urdf = os.path.join(
-        get_package_share_directory("crazyflie"), "urdf", "crazyflie_description.urdf"
+        pgk_crazyflie, "urdf", "crazyflie_description.urdf"
     )
     with open(urdf, "r") as f:
         robot_desc = f.read()
     server_params[1]["robot_description"] = robot_desc
+    
+    num_custom_robot = 1
+    
+    nodes = []
 
+    # Crazyflie server and rviz
     server_node = [
         DeclareLaunchArgument(name='log_level', default_value='debug'),
         DeclareLaunchArgument("backend", default_value="cflib"),
@@ -55,7 +62,7 @@ def generate_launch_description():
             arguments=[
                 "-d"
                 + os.path.join(
-                    get_package_share_directory("crazyflie"), "config", "config.rviz"
+                    pgk_crazyflie, "config", "config.rviz"
                 )
             ],
             parameters=[
@@ -65,42 +72,120 @@ def generate_launch_description():
             ],
         ),
     ]
+    nodes.extend(server_node)
 
+    # spawn Crazyflie robots
     robots_node = []
     time = 0.0
-    for robot_name, robot_info in crazyflies['robots'].items():
-        if str(robot_info['enabled']) == "True":
-            group = GroupAction([
-                Node(
-                    package="crazyflie_controller",
-                    executable="run_many",
-                    name="controller_node",
-                    output="screen",
-                    arguments=[robot_name],
-                ),
-                Node(
-                    package="robot_reader",
-                    executable="robot_reader",
-                    output="screen",
-                    arguments=[robot_name, robot_name],
-                ),
-                Node(
-                    package="robot_writer",
-                    executable="robot_writer",
-                    output='screen',
-                    arguments=[robot_name, robot_name]
-                )
-            ])
-            timed_group = TimerAction(
-                period=time,
-                actions=[group]
+    for i in range(num_custom_robot):
+        robot_namespace = f'worker_{i}'
+        gz_topic = f'/model/{robot_namespace}'
+        joint_state_gz_topic = f'/world/demo{gz_topic}/joint_state'
+        link_pose_gz_topic = f'{gz_topic}/pose'
+
+        # Modified parameter bridge for custom robot
+        bridge = Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            namespace=robot_namespace,
+            arguments=[
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                f'{joint_state_gz_topic}@sensor_msgs/msg/JointState[gz.msgs.Model',
+                f'{link_pose_gz_topic}@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+                f'{gz_topic}/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+                f'{gz_topic}/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+                f'lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                f'lidar/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'
+            ],
+            remappings=[
+                (joint_state_gz_topic, 'joint_states'),
+                (f'{gz_topic}/tf', 'tf'),
+                (link_pose_gz_topic, 'pose'),
+            ],
+            parameters=[
+                {'use_sim_time': True}
+            ],
+            output='screen'
+        )
+
+        # Read custom robot description
+        with open(sdf_robot_file, 'r') as infp:
+            custom_robot_desc = infp.read()
+
+        # Modified robot state publisher for custom robot
+        robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace=robot_namespace,
+            name=f'robot_state_publisher_{i}',
+            output='both',
+            parameters=[
+                {'use_sim_time': True},
+                {'robot_description': custom_robot_desc},
+                {'frame_prefix': f'{robot_namespace}/'},
+                {'publish_frequency': 30.0}
+            ],
+            arguments=['--ros-args', '--log-level', 'debug']
+        )
+        
+        # Add transforms from world to odom and odom to base_link
+        transform_publishers = [
+            # world -> odom
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                namespace=robot_namespace,
+                name=f'world_to_odom_tf_{i}',
+                arguments=[
+                    '--x', '0',
+                    '--y', '0',
+                    '--z', '0',
+                    '--qx', '0',
+                    '--qy', '0',
+                    '--qz', '0',
+                    '--qw', '1',
+                    '--frame-id', 'world',
+                    '--child-frame-id', f'{robot_namespace}/odom'
+                ]
+            ),
+            # odom -> chassis (base_link)
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                namespace=robot_namespace,
+                name=f'odom_to_base_tf_{i}',
+                arguments=[
+                    '--x', '0',
+                    '--y', '0',
+                    '--z', '0',
+                    '--qx', '0',
+                    '--qy', '0',
+                    '--qz', '0',
+                    '--qw', '1',
+                    '--frame-id', f'{robot_namespace}/odom',
+                    '--child-frame-id', f'{robot_namespace}/chassis'
+                ]
+            ),
+            # chassis -> lidar_link
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                namespace=robot_namespace,
+                name=f'base_to_lidar_tf_{i}',
+                arguments=[
+                    '--x', '0.8',
+                    '--y', '0.0',
+                    '--z', '0.51',
+                    '--qx', '0',
+                    '--qy', '0',
+                    '--qz', '0',
+                    '--qw', '1',
+                    '--frame-id', f'{robot_namespace}/chassis',
+                    '--child-frame-id', f'{robot_namespace}/lidar_link'
+                ]
             )
-            time += 3.0
-            robots_node.append(timed_group)
+        ]
+        
+        nodes.extend([bridge, robot_state_publisher] + transform_publishers)
 
-    delay_action_robots = TimerAction(
-            period=7.0,  
-            actions=robots_node
-    )
-
-    return LaunchDescription(server_node + [delay_action_robots])
+    return LaunchDescription(nodes)
--- Crazyflie/run_gazebo_sitl.sh
+++ Crazyflie/run_gazebo_sitl.sh
@@ -1,69 +1,77 @@
 #!/bin/bash
+
 function cleanup() {
-	pkill -x cf2
-	pkill -9 ruby
+    pkill -x cf2
+    pkill -9 ruby
 }
 
-function spawn_model() {
-	MODEL=$1
-	N=$2 # Cf ID
-	X=$3 # spawn x position
-	Y=$4 # spawn y position
-	X=${X:=$X}
-	Y=${Y:=$Y}
-	SUPPORTED_MODELS=("crazyflie", "crazyflie_thrust_upgrade")
-	if [[ " ${SUPPORTED_MODELS[*]} " != *"$MODEL"* ]];
-	then
-		echo "ERROR: Currently only vehicle model $MODEL is not supported!"
-		echo "       Supported Models: [${SUPPORTED_MODELS[@]}]"
-		trap "cleanup" SIGINT SIGTERM EXIT
-		exit 1
-	fi
-	
-	working_dir="$build_path/$n"
-	[ ! -d "$working_dir" ] && mkdir -p "$working_dir"
-
-	pushd "$working_dir" &>/dev/null
-
-	set --
-	set -- ${@} ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo/launch/jinja_gen.py
-	set -- ${@} ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo/models/${MODEL}/model.sdf.jinja
-	set -- ${@} ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo
-	set -- ${@} --cffirm_udp_port $((19950+${N}))
-	set -- ${@} --cflib_udp_port $((19850+${N}))
-	set -- ${@} --cf_id $((${N}))
-	set -- ${@} --cf_name cf
-	set -- ${@} --output-file /tmp/${MODEL}_${N}.sdf
-
-	python3 ${@}
-
-	echo "Spawning ${MODEL}_${N} at ${X} ${Y}"
-
+function spawn_crazyflie() {
+    MODEL=$1
+    N=$2 # Cf ID
+    X=$3 # spawn x position
+    Y=$4 # spawn y position
+    X=${X:=$X}
+    Y=${Y:=$Y}
+    
+    SUPPORTED_MODELS=("crazyflie" "crazyflie_thrust_upgrade")
+    if [[ " ${SUPPORTED_MODELS[*]} " != *"$MODEL"* ]]; then
+        echo "ERROR: Currently vehicle model $MODEL is not supported!"
+        echo " Supported Models: [${SUPPORTED_MODELS[@]}]"
+        trap "cleanup" SIGINT SIGTERM EXIT
+        exit 1
+    fi
+    
+    working_dir="$build_path/$n"
+    [ ! -d "$working_dir" ] && mkdir -p "$working_dir"
+    pushd "$working_dir" &>/dev/null
+    
+    set --
+    set -- ${@} ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo/launch/jinja_gen.py
+    set -- ${@} ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo/models/${MODEL}/model.sdf.jinja
+    set -- ${@} ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo
+    set -- ${@} --cffirm_udp_port $((19950+${N}))
+    set -- ${@} --cflib_udp_port $((19850+${N}))
+    set -- ${@} --cf_id $((${N}))
+    set -- ${@} --cf_name cf
+    set -- ${@} --output-file /tmp/${MODEL}_${N}.sdf
+    python3 ${@}
+    
+    echo "Spawning ${MODEL}_${N} at ${X} ${Y}"
     gz service -s /world/${world}/create --reqtype gz.msgs.EntityFactory --reptype gz.msgs.Boolean --timeout 300 --req 'sdf_filename: "/tmp/'${MODEL}_${N}'.sdf", pose: {position: {x:'${X}', y:'${Y}', z: 0.5}}, name: "'${MODEL}_${N}'", allow_renaming: 1'
-	
-	echo "starting instance $N in $(pwd)"
-	$build_path/cf2 $((19950+${N})) > out.log 2> error.log &
-
-	popd &>/dev/null
+    
+    echo "starting instance $N in $(pwd)"
+    $build_path/cf2 $((19950+${N})) > out.log 2> error.log &
+    popd &>/dev/null
 }
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]
-then
-	echo "Usage: $0 [-n <num_vehicles>] [-m <vehicle_model>] [-w <world>] [-s <script>]"
-	echo "-s flag is used to script spawning vehicles e.g. $0 -s crazyflie:3"
-	exit 1
+function spawn_custom_robot() {
+    MODEL=$1
+    N=$2
+    X=$3
+    Y=$4
+    X=${X:=$X}
+    Y=${Y:=$Y}
+    
+    echo "Spawning ${MODEL}_${N} at ${X} ${Y}"
+    gz service -s /world/${world}/create --reqtype gz.msgs.EntityFactory --reptype gz.msgs.Boolean --timeout 300 --req 'sdf_filename: "robot.sdf", pose: {position: {x:'${X}', y:'${Y}', z: 0.5}}, name: "'${MODEL}_${N}'", allow_renaming: 1'
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: $0 [-n <num_vehicles>] [-m <vehicle_model>] [-w <world>] [-s <script>]"
+    echo "-s flag is used to script spawning vehicles e.g. $0 -s crazyflie:3"
+    exit 1
 fi
 
 while getopts n:m:w:s:t:l: option
 do
-	case "${option}"
-	in
-		m) VEHICLE_MODEL=${OPTARG};;
-		w) WORLD=${OPTARG};;
-		s) SCRIPT=${OPTARG};;
-		t) TARGET=${OPTARG};;
-		l) LABEL=_${OPTARG};;
-	esac
+    case "${option}"
+    in
+        m) VEHICLE_MODEL=${OPTARG};;
+        w) WORLD=${OPTARG};;
+        s) SCRIPT=${OPTARG};;
+        t) TARGET=${OPTARG};;
+        l) LABEL=_${OPTARG};;
+    esac
 done
 
 world=${WORLD:=crazysim_default}
@@ -71,62 +79,59 @@ target=${TARGET:=cf2}
 vehicle_model=${VEHICLE_MODEL:="crazyflie"}
 export CF2_SIM_MODEL=gz_${vehicle_model}
 
-echo ${SCRIPT}
-
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 src_path="$SCRIPT_DIR/crazyflie-firmware"
 build_path=${src_path}/sitl_make/build
 
 echo "killing running crazyflie firmware instances"
 pkill -x cf2 || true
-
 sleep 1
 
 source ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo/launch/setup_gz.bash ${src_path} ${build_path}
 
 echo "Starting gazebo"
-# gz sim -s -r ${src_path}/tools/crazyflie-simulation/simulator_files/gazebo/worlds/${world}.sdf -v 3 &
 gz sim -s -r worlds/${world}.sdf -v 3 &
-
 sleep 3
 
 n=0
+
 if [ -z ${SCRIPT} ]; then
-
-	while IFS= read -r line || [ -n "$line" ];do
-		fields=($(printf "%s" "$line"|cut -d',' --output-delimiter=' ' -f1-))
-		spawn_model ${vehicle_model} $(($n)) ${fields[0]} ${fields[1]}
-		n=$(($n + 1))
-	done < "agents.txt"
-
+    while IFS= read -r line || [ -n "$line" ]; do
+        fields=($(printf "%s" "$line"|cut -d',' --output-delimiter=' ' -f1-))
+        robot_type=${fields[2]:-"crazyflie"}  # If field[2] is empty, default to crazyflie
+        
+        if [ "$robot_type" = "crazyflie" ]; then
+            spawn_crazyflie ${vehicle_model} $(($n)) ${fields[0]} ${fields[1]}
+        else
+            spawn_custom_robot ${robot_type} $(($n)) ${fields[0]} ${fields[1]}
+        fi
+        n=$(($n + 1))
+    done < "agents.txt"
 else
-	IFS=,
-	for target in ${SCRIPT}; do
-		target="$(echo "$target" | tr -d ' ')" #Remove spaces
-		target_vehicle=$(echo $target | cut -f1 -d:)
-		target_number=$(echo $target | cut -f2 -d:)
-		target_x=$(echo $target | cut -f3 -d:)
-		target_y=$(echo $target | cut -f4 -d:)
-
-		if [ $n -gt 255 ]
-		then
-			echo "Tried spawning $n vehicles. The maximum number of supported vehicles is 255"
-			exit 1
-		fi
-
-		m=0
-		while [ $m -lt ${target_number} ]; do
-			export CF2_SIM_MODEL=gz_${target_vehicle}
-			spawn_model ${target_vehicle}${LABEL} $(($n)) $target_x $target_y
-			m=$(($m + 1))
-			n=$(($n + 1))
-		done
-	done
-
+    IFS=,
+    for target in ${SCRIPT}; do
+        target="$(echo "$target" | tr -d ' ')" #Remove spaces
+        target_vehicle=$(echo $target | cut -f1 -d:)
+        target_number=$(echo $target | cut -f2 -d:)
+        target_x=$(echo $target | cut -f3 -d:)
+        target_y=$(echo $target | cut -f4 -d:)
+        
+        if [ $n -gt 255 ]; then
+            echo "Tried spawning $n vehicles. The maximum number of supported vehicles is 255"
+            exit 1
+        fi
+        
+        m=0
+        while [ $m -lt ${target_number} ]; do
+            export CF2_SIM_MODEL=gz_${target_vehicle}
+            spawn_crazyflie ${target_vehicle}${LABEL} $(($n)) $target_x $target_y
+            m=$(($m + 1))
+            n=$(($n + 1))
+        done
+    done
 fi
+
 trap "cleanup" SIGINT SIGTERM EXIT
 
 echo "Starting gazebo gui"
-#gdb ruby
 gz sim -g
